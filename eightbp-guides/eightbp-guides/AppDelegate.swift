dac550ca29704756e3b65fbb4b9b6b6f94c6e953
Helmut Januschka <helmut@januschka.com>
2017-07-04 15:12:27 +0200

    up

---
+++ eightbp-guides/eightbp-guides/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  eightbp-guides
+//
+//  Created by Januschka Helmut on 04.07.17.
+//  Copyright © 2017 hjanuschka. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ eightbp-guides/eightbp-guides/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  eightbp-guides
+//
+//  Created by Januschka Helmut on 04.07.17.
+//  Copyright © 2017 hjanuschka. All rights reserved.
+//
+
+import Cocoa
+
+final class Line: NSView {
+    var mouse: CGPoint?
+    
+    func drawLine(x: CGFloat, y: CGFloat) {
+        let myPath = NSBezierPath()
+        myPath.move(to: mouse!)
+        myPath.line(to: CGPoint(x: x, y: y))
+        myPath.stroke()
+    }
+    override func draw(_ dirtyRect: NSRect) {
+        
+        //Left Bottom
+        self.drawLine(x: 0, y: 0);
+        //Bottom Center
+        self.drawLine(x: frame.size.width/2, y: 0);
+        //Bottom Right
+        self.drawLine(x: frame.size.width, y: 0);
+        
+        //Top Left
+        self.drawLine(x: 0, y: frame.size.height);
+        //Top Middle
+        self.drawLine(x: frame.size.width/2, y: frame.size.height);
+        //Top Right
+        self.drawLine(x: frame.size.width, y: frame.size.height);
+        
+        
+        let circle = NSBezierPath(ovalIn: NSRect(x: (mouse?.x)!-10, y: (mouse?.y)!-10, width: 20, height: 20))
+        circle.stroke()
+        circle.fill()
+        
+        //self.drawLine(x: 0, y: 0);
+        
+        
+        
+    }
+}
+
+class ViewController: NSViewController, NSWindowDelegate {
+    var currentCenter: NSPoint = CGPoint(x:0, y:0);
+    var Lines:Line?
+    
+    override func flagsChanged(with event: NSEvent) {
+        switch event.modifierFlags.intersection(.deviceIndependentFlagsMask) {
+        case [.shift]:
+            print("shift key is pressed")
+        case [.control]:
+            print("control key is pressed")
+        case [.option] :
+            print("option key is pressed")
+        case [.command]:
+            print("Command key is pressed")
+            view.window?.ignoresMouseEvents=false;
+        case [.control, .shift]:
+            print("control-shift keys are pressed")
+        case [.option, .shift]:
+            print("option-shift keys are pressed")
+        case [.command, .shift]:
+            print("command-shift keys are pressed")
+        case [.control, .option]:
+            print("control-option keys are pressed")
+        case [.control, .command]:
+            print("control-command keys are pressed")
+        case [.option, .command]:
+            print("option-command keys are pressed")
+        case [.shift, .control, .option]:
+            print("shift-control-option keys are pressed")
+        case [.shift, .control, .command]:
+            print("shift-control-command keys are pressed")
+        case [.control, .option, .command]:
+            print("control-option-command keys are pressed")
+        case [.shift, .command, .option]:
+            print("shift-command-option keys are pressed")
+        case [.shift, .control, .option, .command]:
+            print("shift-control-option-command keys are pressed")
+        default:
+            view.window?.ignoresMouseEvents=true;
+            print("no modifier keys are pressed")
+        }
+    }
+    override func mouseDown(with event: NSEvent) {
+        let p = event.locationInWindow;
+        NSLog("%@", NSStringFromPoint(p));
+        currentCenter = p;
+        Lines?.mouse = p;
+        Lines?.needsDisplay=true;
+    }
+    
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        view.window?.isOpaque = false
+        view.window?.backgroundColor = .clear
+        view.window?.delegate = self
+        view.window?.ignoresMouseEvents=true;
+        self.becomeFirstResponder()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let frame = self.view.frame;
+        self.Lines = Line(frame: frame)
+        self.Lines?.mouse = CGPoint(x: view.frame.size.height/2, y: view.frame.size.width/2);
+        view.addSubview(self.Lines!)
+        // Do any additional setup after loading the view.
+        
+        NSApplication.shared().windows.first?.level = Int(CGWindowLevelForKey(.floatingWindow))
+        
+        
+        NSEvent.addLocalMonitorForEvents(matching: .flagsChanged) {
+            self.flagsChanged(with: $0)
+            return $0
+        }
+        
+    }
+    func windowDidResize(_ notification: Notification) {
+        // Your code goes here
+        NSLog("AAA")
+        self.Lines?.frame = view.frame;
+        self.Lines?.needsDisplay=true;
+    }
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
